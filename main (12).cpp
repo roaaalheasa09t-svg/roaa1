d4ec6eb77f2fce5baf4378a8f4b5d77b20147906
roaa hiasat <roaaalheasa09t@gmail.com>
2026-03-13 19:39:55 +0300

    Add files via upload

---
+++ main (12).cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+string yo="roaa\n";
+cin>>yo;
+cout<<"my name is "<<yo<<endl;
+cout<<5*6<<endl;
+
+int x=5;
+int y=4;
+cout<<x/y<<endl;//الجواب هون 1
+
+
+double u=5;
+double b=4;
+cout<<u/b<<endl;//الجواب هون 1.25
+
+
+int r=3;
+int o=5;
+r=9;
+o=4;
+cout<<o/r<<endl;// الجواب صفر لانه انتجر
+
+
+
+
+double p=3;
+double q=5;
+p=9;
+q=4;
+cout<<q/p<<endl;//رقم عشري لانه دبل
+
+
+int c=3;
+int v=4;
+int z=2;
+c=6;
+v=9;
+cin>>c;
+cin>>v;
+z=c+v;
+cout<<z<<endl;//الجواب 11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
