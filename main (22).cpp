c9f9120e0547a215d6406a93f2257d26c44c2e96
roaa hiasat <roaaalheasa09t@gmail.com>
2026-04-09 12:22:00 +0300

    Add files via upload

---
+++ main (22).cpp
@@ -0,0 +1,127 @@
+ 
+#include <iostream>
+using namespace std;
+int main()
+{
+/*
+  int a=10;
+  int z;
+  
+  if(a==10)
+  {
+      cout<<"it is 10\n";
+      
+  }
+   else if (a==11)
+{
+    cout<<"it is 11\n";
+
+}
+else if (z==13)
+{
+    cout<<"it is 13\n";
+}
+else 
+{
+    cout<<"good bye\n";
+}
+*/
+
+
+
+
+/*int x=10;            حل 1
+if(x>=2)
+{
+    cout<<"big 2\n";
+}
+else
+{
+    cout<<"thank you\n"; 
+    
+}*/
+
+
+
+
+/*
+int x=11;
+(x>=2)?cout<<"greater 2\n":cout<<"thank you\n"; // حل 2
+*/
+
+
+
+
+
+/*int a=100;
+int b=200;
+
+if(a==100)// هون الشرط تحقق فعشان هيك دخلنا لجوا انستد اف والشرط برضو تحقق
+//لو كانت غير متحققه ما بندخل على انستد اف 
+{
+    if(b==200) 
+    {
+        cout<<"value 200\n";     //رح يعطيني هاد الجواب 
+    }*/
+
+   /* 
+int a=100;
+int b=200;
+if(a==100)
+{
+    if(b==201) 
+    {
+        cout<<"value 200\n"; // ما رح يعطيني جواب لانه ما تحقق الشرط 
+    
+    }
+    else if (b==100)
+    {
+        cout<<"roaa\n";
+    }
+    else
+    cout<<"good bye\n";// رح يطبع هاد الجواب 
+    
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
