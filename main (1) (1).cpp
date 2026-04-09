dd42bfbc62ecab525a997ddf3d2280c70c64f9db
roaa hiasat <roaaalheasa09t@gmail.com>
2026-04-09 12:21:40 +0300

    Add files via upload

---
+++ main (1) (1).cpp
@@ -0,0 +1,126 @@
+// program to chek whether an integer is positive,negative or zero  رقم صحيح موجب او سالب او صفر
+#include <iostream>
+using namespace std;
+int main()
+{
+   /* int num;
+    cout<<"Enter the number \n";
+    cin>>num;
+    
+    if(num>0)
+    {
+      cout<<"positive\n";  
+    }
+    else if(num<0)
+     {
+    cout<<"negative\n";
+    
+     }
+     else 
+     {
+     cout<<"Zero\n";
+        
+     }
+
+*/
+
+
+
+
+
+/*
+//برنامج يطبع علامات الطلاب باستخدام انستد اف
+int gr;
+cout<<"Enter the grade\n";
+cin>>gr;
+if (gr>49&&gr!=0)
+{
+     if(gr>=50&&gr<=60)//لازم يتحقق الشرطين مشان يطبع
+{
+    cout<<"D\n";
+
+}
+else if (gr>60&&gr<=70)
+{
+    cout<<"C\n";
+}
+else if (gr>70&&gr<=80)
+{
+    cout<<"B\n";
+}
+else if(gr>80&&gr<=90)
+{
+    cout<<"B+\n";
+}
+else if(gr>90&&gr<=100)
+{
+    cout<<"A\n";
+}
+else
+{
+    cout<<"above 100\n";
+}
+}  
+else 
+{
+    cout<<"under 50\n";
+}
+
+*/
+
+
+
+
+/*
+// برنامج يعمل تحقق للرقم فردي او زوجي 
+
+int num;
+cout<<"Enter the number\n";
+cin>>num;
+
+if(num!=0)
+{
+if(num%2==0)
+{
+    cout<<"the even\n";
+}
+
+else
+{
+    cout<<"the odd\n";
+}
+}
+else
+{
+    cout<<"Zero\n";
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
